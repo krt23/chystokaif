da1fa8bc4d2a6b15a1922bd489d5c787d5093eee
Kristina Tkachyk <tkachyk.kris@gmail.com>
2020-09-07 16:54:32 +0300

    build

---
+++ precache-manifest.8ee41e58a0710f3f965ae091f0eafb31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6920d74fc4a75ecd24bfd428563372da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1d6fc3dd085a824483",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "304a66ffaf1d1cd1c408",
+    "url": "/static/js/2.a1fe0ff6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1fe0ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1d6fc3dd085a824483",
+    "url": "/static/js/main.d1fa2661.chunk.js"
+  },
+  {
+    "revision": "cac22dd7a1a408f43d09",
+    "url": "/static/js/runtime-main.dfdc1305.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
